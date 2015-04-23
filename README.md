27a6a0e6eece02ada4b5c521facc9af9f35ba5c2
Yufei Li <yufeiminds@gmail.com>
2015-04-23 23:45:46 +0800

    Init plugin.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # itoc
+
 It is a jQuery plugin, A toc tree generator from html document.
+
+This repo is modify by a jquery plugin [toc](https://github.com/jgallen23/toc) .
+
+The toc plugin document please visit [here](http://projects.jga.me/toc/) .
+
+## Install
+
+You can get the source from github, or install it by bower.
+
+### Download source
+
+    git clone https://github.com/yufeiminds/itoc.js
+
+### Bower
+
+    bower install itoc.js
+
+## Usage
+
+### Default settings
+
+```javascript
+    var defaults = {
+        'selectors': 'h2, h3',
+        'statistic': { H2: 0, H3: 0, H4: 0},
+        'auto_num': true,
+        'symbol_toc': {
+            H2: ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'],
+            H3: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            H4: ['0)', '1)', '2)', '3)', '4)', '5)', '6)', '7)', '8)', '9)'],
+        }
+    }
+```
+
+### Init
+
+```javascript
+    $(document).ready( function() {
+        $('.toc').itoc({
+            /* Your custom settings */
+        });
+    });
+```
+
+## Contribute
+
+1. Fork this repo.
+2. Modify it.
+3. Create a pull request.
+
+happy coding...!
+
+## Contact
+
+**Email**
+
+yufeiminds@gmail.com
---
+++ itoc.js
@@ -0,0 +1,251 @@
+/*!
+ * toc - jQuery Table of Contents Plugin
+ * v0.3.2
+ * http://projects.jga.me/toc/
+ * copyright Greg Allen 2014
+ * MIT License
+*/
+
+/*!
+ * smooth-scroller - Javascript lib to handle smooth scrolling
+ * v0.1.2
+ * https://github.com/firstandthird/smooth-scroller
+ * copyright First+Third 2014
+ * MIT License
+*/
+
+/*!
+ * itoc - Javascript lib to handle smooth scrolling
+ * Plugin: itoc
+ * Dependence: toc
+ * Author: Yufei Li
+ * Time: 2015-4-21
+*/
+
+(function($) {
+  $.fn.smoothScroller = function(options) {
+    options = $.extend({}, $.fn.smoothScroller.defaults, options);
+    var el = $(this);
+
+    $(options.scrollEl).animate({
+      scrollTop: el.offset().top - $(options.scrollEl).offset().top - options.offset
+    }, options.speed, options.ease, function() {
+      var hash = el.attr('id');
+
+      if(hash.length) {
+        if(history.pushState) {
+          history.pushState(null, null, '#' + hash);
+        } else {
+          document.location.hash = hash;
+        }
+      }
+
+      el.trigger('smoothScrollerComplete');
+    });
+
+    return this;
+  };
+
+  $.fn.smoothScroller.defaults = {
+    speed: 400,
+    ease: 'swing',
+    scrollEl: 'body,html',
+    offset: 0
+  };
+
+  $('body').on('click', '[data-smoothscroller]', function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+
+    if(href.indexOf('#') === 0) {
+      $(href).smoothScroller();
+    }
+  });
+}(jQuery));
+
+(function($) {
+var verboseIdCache = {};
+$.fn.toc = function(options) {
+  var self = this;
+  var opts = $.extend({}, jQuery.fn.toc.defaults, options);
+
+  var container = $(opts.container);
+  var headings = $(opts.selectors, container);
+  var headingOffsets = [];
+  var activeClassName = opts.activeClass;
+
+  var scrollTo = function(e, callback) {
+    if (opts.smoothScrolling && typeof opts.smoothScrolling === 'function') {
+      e.preventDefault();
+      var elScrollTo = $(e.target).attr('href');
+
+      opts.smoothScrolling(elScrollTo, opts, callback);
+    }
+    $('li', self).removeClass(activeClassName);
+    $(e.target).parent().addClass(activeClassName);
+  };
+
+  //highlight on scroll
+  var timeout;
+  var highlightOnScroll = function(e) {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(function() {
+      var top = $(window).scrollTop(),
+        highlighted, closest = Number.MAX_VALUE, index = 0;
+      
+      for (var i = 0, c = headingOffsets.length; i < c; i++) {
+        var currentClosest = Math.abs(headingOffsets[i] - top);
+        if (currentClosest < closest) {
+          index = i;
+          closest = currentClosest;
+        }
+      }
+      
+      $('li', self).removeClass(activeClassName);
+      highlighted = $('li:eq('+ index +')', self).addClass(activeClassName);
+      opts.onHighlight(highlighted);      
+    }, 50);
+  };
+  if (opts.highlightOnScroll) {
+    $(window).bind('scroll', highlightOnScroll);
+    highlightOnScroll();
+  }
+
+  return this.each(function() {
+    //build TOC
+    var el = $(this);
+    var ul = $(opts.listType);
+
+    headings.each(function(i, heading) {
+      var $h = $(heading);
+      headingOffsets.push($h.offset().top - opts.highlightOffset);
+
+      var anchorName = opts.anchorName(i, heading, opts.prefix);
+
+      //add anchor
+      if(heading.id !== anchorName) {
+        var anchor = $('<span/>').attr('id', anchorName).insertBefore($h);
+      }
+
+      //build TOC item
+      var a = $('<a/>')
+        .text(opts.headerText(i, heading, $h))
+        .attr('href', '#' + anchorName)
+        .bind('click', function(e) {
+          $(window).unbind('scroll', highlightOnScroll);
+          scrollTo(e, function() {
+            $(window).bind('scroll', highlightOnScroll);
+          });
+          el.trigger('selected', $(this).attr('href'));
+        });
+
+      var li = $('<li/>')
+        .addClass(opts.itemClass(i, heading, $h, opts.prefix))
+        .append(a);
+
+      ul.append(li);
+    });
+    el.html(ul);
+  });
+};
+
+
+jQuery.fn.toc.defaults = {
+  container: 'body',
+  listType: '<ul/>',
+  selectors: 'h1,h2,h3',
+  smoothScrolling: function(target, options, callback) {
+    $(target).smoothScroller({
+      offset: options.scrollToOffset
+    }).on('smoothScrollerComplete', function() {
+      callback();
+    });
+  },
+  scrollToOffset: 0,
+  prefix: 'toc',
+  activeClass: 'toc-active',
+  onHighlight: function() {},
+  highlightOnScroll: true,
+  highlightOffset: 100,
+  anchorName: function(i, heading, prefix) {
+    if(heading.id.length) {
+      return heading.id;
+    }
+
+    var candidateId = $(heading).text().replace(/[^a-z0-9]/ig, ' ').replace(/\s+/g, '-').toLowerCase();
+    if (verboseIdCache[candidateId]) {
+      var j = 2;
+      
+      while(verboseIdCache[candidateId + j]) {
+        j++;
+      }
+      candidateId = candidateId + '-' + j;
+      
+    }
+    verboseIdCache[candidateId] = true;
+
+    return prefix + '-' + candidateId;
+  },
+  headerText: function(i, heading, $heading) {
+    return $heading.text();
+  },
+  itemClass: function(i, heading, $heading, prefix) {
+    return prefix + '-' + $heading[0].tagName.toLowerCase();
+  }
+
+};
+
+})(jQuery);
+
+/*******************************
+* Plugin: itoc
+* Dependence: toc
+* Author: Yufei Li
+* Time: 2015年4月21日
+********************************/
+
+;(function($) {
+    $.fn.itoc = function(options) {
+        /* Merge arguments */
+        var opts = $.extend({}, $.fn.itoc.defaults, options);
+        
+        /* Header Text */
+        if (opts.auto_num !== false) {
+            console.log(opts.auto_num);
+            console.log(opts.auto_num !== false);
+           opts.headerText = function(i, heading, $heading) {
+                var tag_name = jQuery(heading).prop('tagName');
+                opts.statistic[tag_name] += 1;
+
+                /* 小于此标签范围置零 */
+                for (tag in opts.statistic) {
+                    if (tag > tag_name) {
+                        opts.statistic[tag] = 0;
+                    }
+                }
+
+                var text = jQuery(heading).text();
+                var symbol = opts.symbol_toc[tag_name][opts.statistic[tag_name]];
+                jQuery(heading).text(symbol + '. ' + text);
+                return $heading.text();
+            } 
+        }
+
+        /* Bind header-text to every title. */
+        $(this).toc(opts);
+    }
+    
+    $.fn.itoc.defaults = {
+        'selectors': 'h2, h3',
+        'statistic': { H2: 0, H3: 0, H4: 0},
+        'auto_num': true,
+        'symbol_toc': {
+            H2: ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'],
+            H3: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            H4: ['0)', '1)', '2)', '3)', '4)', '5)', '6)', '7)', '8)', '9)'],
+        }
+    };
+    
+})(jQuery);
